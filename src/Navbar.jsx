f8e391f5305fdd7404d9b5e61b8577a83689cf05
Prashant Singhal <6prashant1singhal9@gmail.com>
2020-03-30 10:59:46 +0530

    Style navigation bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,25 +6,34 @@ const Navbar = () => {
   return (
     <nav className="Navbar">
       <ul className="Navbar-nav">
+        <li className="logo">
+          <a href="#" className="nav-link">
+            <span className="link-text logo-text">Tour The World</span>
+            <i className="fas fa-angle-double-right"></i>
+          </a>
+        </li>
         <li id="home" className="nav-item">
           <Link
+            className="nav-link"
             to={{
               pathname: "/",
               state: { status: "homeActive" }
             }}
           >
-            <i className="homeIcon fa-4x fas fa-igloo" />
-            Home
+            <i className="home-icon fa-2x fas fa-igloo" />
+            <span className="link-text">Home</span>
           </Link>
         </li>
         <li id="favourite" className="nav-item">
           <Link
+            className="nav-link"
             to={{
               pathname: "/favourite-places",
               state: { status: "favouriteActive" }
             }}
           >
-            Favourite Places
+            <i className="favourite-icon fa-2x fas fa-heart" />
+            <span className="link-text">Favourite Places</span>
           </Link>
         </li>
       </ul>

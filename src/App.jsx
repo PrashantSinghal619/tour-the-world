9bda7a9cf14f43a707fadfec30969dbec8de4114
Prashant Singhal <6prashant1singhal9@gmail.com>
2020-03-30 12:54:39 +0530

    Add dummy image sources

--- src/App.jsx
+++ src/App.jsx
@@ -16,55 +16,55 @@ function App() {
     {
       name: "The Alps",
       description: "Do you dare take the hike?",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/xWxh7hsgJVA/300x200",
       favourite: checkFavourite("The Alps")
     },
     {
       name: "Nepal",
       description: "Land of the enlightened",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/xyE1p1rG04U/300x200",
       favourite: checkFavourite("Nepal")
     },
     {
       name: "Hawaii",
       description: "Get lost in the sea",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/U_D5NWMmTTM/300x200",
       favourite: checkFavourite("Hawaii")
     },
     {
       name: "Malaysia",
       description: "Truly Asia",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/FiPot-1rsZw/300x200",
       favourite: checkFavourite("Malaysia")
     },
     {
       name: "Australia",
       description: "Country down under",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/qaNcz43MeY8/300x200",
       favourite: checkFavourite("Australia")
     },
     {
       name: "Mariana Trench",
       description: "How deep can you go?",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/94lCSSQyTrw/300x200",
       favourite: checkFavourite("Mariana Trench")
     },
     {
       name: "Scandinavia",
       description: "Chill to the core",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/tq40ATQPbgY/300x200",
       favourite: checkFavourite("Scandinavia")
     },
     {
       name: "Dubai",
       description: "As-salamu alaykum",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/fCW1hWq2nq0/300x200",
       favourite: checkFavourite("Dubai")
     },
     {
       name: "Machu Pichu",
       description: "Rediscover the beauty within this ancient city's ruins",
-      imageSource: "",
+      imageSource: "https://source.unsplash.com/PO7CGnoDFUI/300x200",
       favourite: checkFavourite("Machu Pichu")
     }
   ]);
